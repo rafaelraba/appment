b5dad624d66ddd21bcbf3c808fd282e97e1a439a
Rafael Raba <rafaelrb40@gmail.com>
2023-03-13 20:11:45 -0500

    update test

--- tests/Contexts/Dosmi/Preachers/application/PreacherCreator.test.ts
+++ tests/Contexts/Dosmi/Preachers/application/PreacherCreator.test.ts
@@ -1,7 +1,6 @@
 import { PreacherCreator } from '../../../../../src/Contexts/Dosmi/Preachers/application/create/PreacherCreator';
 import { Preacher } from '../../../../../src/Contexts/Dosmi/Preachers/domain/Preacher';
 import { PreacherRepositoryMock } from '../__mocks__/PreacherRepositoryMock';
-import { Uuid } from "../../../../../src/Contexts/Shared/domain/value-object/Uuid";
 
 let repository: PreacherRepositoryMock;
 let creator: PreacherCreator;
@@ -13,7 +12,7 @@ beforeEach(() => {
 
 describe('PreacherCreator', () => {
   it('should create a valid preacher', async () => {
-    const id = new Uuid('7fc35b52-8b4c-40dd-bc2b-9de11c6c2258');
+    const id = '22bad62c-53d2-4594-9a37-3d92b1b96d6a'
     const name = 'some-name';
     const gender = 'some-gender';
     const privilege = 'some-privilege';
@@ -24,7 +23,7 @@ describe('PreacherCreator', () => {
 
     const preacher = new Preacher({ id, name, type, state, gender, birthdate, baptismDate, privilege });
 
-    await creator.run({ id: id.value, name, type, state, gender, birthdate, baptismDate, privilege });
+    await creator.run({ id, name, type, state, gender, birthdate, baptismDate, privilege });
 
     repository.assertLastSavedPreacherIs(preacher);
   });
