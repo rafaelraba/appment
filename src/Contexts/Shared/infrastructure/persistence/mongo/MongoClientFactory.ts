2ae5c30aa7514a907f0e217b68a0863a8c68a6c1
Rafael Raba <rafaelrb40@gmail.com>
2023-05-09 14:10:49 -0500

    MongoClientFactory

---
+++ src/Contexts/Shared/infrastructure/persistence/mongo/MongoClientFactory.ts
@@ -0,0 +1,33 @@
+import { MongoClient } from 'mongodb';
+import { MongoConfig } from './MongoConfig';
+
+export class MongoClientFactory {
+  private static clients: { [key: string]: MongoClient } = {};
+
+  static async createClient(contextName: string, config: MongoConfig) {
+    let client = MongoClientFactory.getClient(contextName);
+
+    if (!client) {
+      client = await MongoClientFactory.createAndConnectClient(config);
+      MongoClientFactory.registerClient(client, contextName);
+    }
+
+    return client;
+  }
+
+  private static getClient(contextName: string) {
+    return MongoClientFactory.clients[contextName];
+  }
+
+  private static async createAndConnectClient(config: MongoConfig) {
+    const client = new MongoClient(config.url);
+
+    await client.connect();
+
+    return client;
+  }
+
+  private static registerClient(client: MongoClient, contextName: string) {
+    MongoClientFactory.clients[contextName] = client;
+  }
+}
